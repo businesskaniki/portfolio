acbe5c7ca922b7d7f0835d2a323ae55d5427c653
norbiboy1 <norbiateniszes@yahoo.com>
2022-06-20 15:48:24 +0300

    Add JavaScript functionality

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
-let nav_item = document.querySelector('#nav-item');
+const menu = document.querySelector('.nav-item');
+const menuItems = document.querySelector('.menu-item');
+const hamburger_button = document.querySelector('#humb');
+const close_button = document.querySelector('.close-btn');
+const menuIcon = document.querySelector('.menuIcon');
+const closeIcon = document.querySelector('.closeIcon');
+
 function onClick() {
-   
-    const hamburger_button = document.querySelector('#humb');
-    if(hamburger_button.style.display == 'block') {
-        nav_item.style.display = 'none';
-    } else {
-       nav_item.style.display = 'block';
+        if (menu.classList.contains("showMenu")) {
+          menu.classList.remove("showMenu");
+          closeIcon.style.display = "none";
+          menuIcon.style.display = "block";
+        } else {
+          menu.classList.add("showMenu");
+          closeIcon.style.display = "block";
+          menuIcon.style.display = "none";
+        }
     }
-}
-function close(){
-    let close_btn = document.querySelector('#close-btn');
-    if(close_btn.style.display=='block'){
-        nav_item.style.display = 'none';
-    }
-}
+      hamburger_button.addEventListener("click", onClick);
+      
+      menuItems.forEach( 
+        function(menuItem) { 
+          menuItem.addEventListener("click", onClick);
+        }
+      )
