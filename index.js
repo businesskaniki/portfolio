811de56ac73e3e3df76644f73931734603dc7a2c
businesskaniki <businesskaniki@gmail.com>
2022-06-22 11:45:43 +0300

    fixing linter errors

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Humb = document.querySelector('#humb');
 const Menu = document.querySelector('.nav-ul');
 
-Humb.addEventListener('click', () =>{
+Humb.addEventListener('click', () => {
   Humb.classList.toggle('active');
   Menu.classList.toggle('active');
 });
