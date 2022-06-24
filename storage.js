33c1d296d09cdde92464c1fde69ef7850e46c8a1
businesskaniki <businesskaniki@gmail.com>
2022-06-24 23:28:47 +0300

    local data storage functionality

---
+++ storage.js
@@ -0,0 +1,25 @@
+const userName = document.getElementById('user-name');
+const userEmail = document.getElementById('user-email');
+const userText = document.getElementById('user-text');
+const contactForm = document.querySelector('form');
+const resetBtn = document.querySelector('.form-reset');
+
+function storeFormData() {
+  const formData = {
+    name: userName.value,
+    email: userEmail.value,
+    text: userText.value,
+  };
+  localStorage.setItem('form', JSON.stringify(formData));
+}
+
+contactForm.addEventListener('change', storeFormData);
+
+function populateForm() {
+  const getData = JSON.parse(localStorage.getItem('form'));
+  if (getData === null) return;
+
+  userName.value = getData.name;
+  userEmail.value = getData.email;
+  userText.value = getData.text;
+}
